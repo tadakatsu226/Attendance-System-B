3529c686ff1647997d6c4a1e702e49397b62599a
EC2 Default User <ec2-user@ip-172-31-47-206.ap-northeast-1.compute.internal>
2020-04-16 14:19:25 +0000

    attedance1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200416075422) do
+ActiveRecord::Schema.define(version: 20200416135811) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20200416075422) do
     t.string "department"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.datetime "basic_time"
+    t.datetime "work_time"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
