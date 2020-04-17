f3f3f39209b8a2e2c3eb0da6a836c85f15f1fdfd
EC2 Default User <ec2-user@ip-172-31-47-206.ap-northeast-1.compute.internal>
2020-04-17 17:00:27 +0000

    Finish edit-attendance

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,12 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   
   resources :users do
-    resources :attendances
+     member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+      get 'attendances/edit_one_month'
+      patch 'attendances/update_one_month'
+    end
+    resources :attendances, only: :update
   end
 end
