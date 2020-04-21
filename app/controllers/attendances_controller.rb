e4b5dcc39f93b41be99565a43a124267e32a1ae3
EC2 Default User <ec2-user@ip-172-31-47-206.ap-northeast-1.compute.internal>
2020-04-21 14:52:40 +0000

    テスト終了

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,8 +1,8 @@
 class AttendancesController < ApplicationController
   
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
-  # before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
 
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
@@ -45,22 +45,22 @@ class AttendancesController < ApplicationController
     flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
     redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
-
+  
+  
+  def admin_or_correct_user
+    @user = User.find(params[:user_id]) if @user.blank?
+      unless current_user?(@user) || current_user.admin?
+        flash[:danger] = "編集権限がありません。"
+        redirect_to(root_url)
+      end
+  end
+   
   
   private
   
     def attendances_params
       params.require(:user).permit(attendances: [:started_at, :finished_at, :note])[:attendances]
     end
-    
-    
-    # 管理権限者、または現在ログインしているユーザーを許可します。
-    # def admin_or_correct_user
-    #   @user = User.find(params[:user_id]) if @user.blank?
-    #   unless current_user?(@user) || current_user.admin?
-    #     flash[:danger] = "編集権限がありません。"
-    #     redirect_to(root_url)
-    #   end  
-    # end
+
 end
 
